75466c322fba792fecfb213704962f3bedef8fac
Zack Myers <42181342+myers268@users.noreply.github.com>
2025-02-23 17:53:44 -0500

    Support unique event payloads (#1)
    Rather than attempting to combine all payload types into a single object, each GameAction is now associated with a unique struct which can define its own payload. All event structs are combined into a single EventUnion struct on the Event field of the Message. Making each field type a pointer effectively makes them optional types; this means on the frontend we need only send the field we want in our payload and on the backend we can simply update the switch statement to match on non-null cases.
    
    Additionally, I have made all of the existing enums implement the String() interface and added a bit of polish to the log outputs, as well as resolved a couple breakages I ran into along the way.

--- packages/ichi/internal/drei.go
+++ packages/ichi/internal/drei.go
@@ -16,6 +16,20 @@ const (
 	Seven
 )
 
+var cardFaceNames = map[CardFace]string{
+	One:   "One",
+	Two:   "Two",
+	Three: "Three",
+	Four:  "Four",
+	Five:  "Five",
+	Six:   "Six",
+	Seven: "Seven",
+}
+
+func (cf CardFace) String() string {
+	return cardFaceNames[cf]
+}
+
 var NumberFaces = []CardFace{One, Two, Three, Four, Five, Six, Seven}
 //var SpecialFaces = []CardFace{Skip, Reverse, DrawTwo, Wild, WildDrawFour}
 
@@ -29,19 +43,34 @@ const (
 	White
 )
 
-var CardColors = []Color{Red, Yellow, Blue, Black, White}
+var colorNames = map[Color]string{
+	Red:    "Red",
+	Yellow: "Yellow",
+	Blue:   "Blue",
+	Black:  "Black",
+	White:  "White",
+}
 
+func (c Color) String() string {
+	return colorNames[c]
+}
+
+var CardColors = []Color{Red, Yellow, Blue, Black, White}
 
 type Card struct {
 	Face  CardFace
 	Color Color
 }
 
+func (c Card) String() string {
+	return c.Color.String() + " " + c.Face.String()
+}
+
 func Compare(c1, c2 Card) bool {
 	return c1.Face == c2.Face || c1.Color == c2.Color
 }
 
-func NewDeck () []Card {
+func NewDeck() []Card {
 	deck := make([]Card, 0)
 	for color := 0; color < len(CardColors); color++ {
 		for face := 0; face < len(NumberFaces); face++ {
